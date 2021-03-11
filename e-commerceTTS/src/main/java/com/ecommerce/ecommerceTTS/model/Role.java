32ab58c7b13183220742e06e0b378f959c3ee4e3
jwdukes <jwdukes@gmail.com>
2021-03-11 15:56:52 -0600

    initial commit

---
+++ e-commerceTTS/src/main/java/com/ecommerce/ecommerceTTS/model/Role.java
@@ -0,0 +1,26 @@
+package com.ecommerce.ecommerceTTS.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Entity
+    public class Role {
+        // this here allows the Id to be auto generated
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(name = "role_id")
+        private Long id;
+        private String role;
+    }
+
+
+
